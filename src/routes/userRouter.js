44fbd185f11f0d01b9be4ddff531224d0e9fc1c7
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-20 19:57:41 -0300

    FEAT [Middleware]: Create a validate middleware for signup route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { logoff, signIn, signUp } from "../controllers/user.controller.js";
-import signUpValidate from "../middleware/signInValidate.js";
+import signInValidate from "../middleware/signInValidate.js";
+import signUpValidate from "../middleware/signUpValidate.js";
 
 const userRouter = Router();
 
-userRouter.post('/sign-in', signUpValidate, signIn);
-userRouter.post('/sign-up', signUp);
+userRouter.post('/sign-in', signInValidate, signIn);
+userRouter.post('/sign-up', signUpValidate, signUp);
 userRouter.delete('logoff', logoff);
 
 export default userRouter;
