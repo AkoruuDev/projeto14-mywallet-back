07b5f9954f8d50ca4f44e325d9b9586042acae15
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-20 20:03:45 -0300

    FEAT [Middleware]: Create a validate middleware for signup route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 import { usersCollection, logCollection } from "../app.js";
-import { userSchema, registerSchema } from "../tools/JoiSchema.js";
 
 export async function signIn (req, res) {
     const { email } = req.body;
@@ -20,22 +19,7 @@ export async function signIn (req, res) {
 export async function signUp(req, res) {
     const { name, email, password } = req.body;
 
-    const { error } = registerSchema.validate({ name, email, password });
-
-    if (error) {
-        res.status(422).send(error.details.map(error => error.message));
-        return;
-    }
-
-    const registerExists = await usersCollection.findOne({ email });
-
-    if (registerExists) {
-        res.status(422).send('Este email já está cadastrado');
-        return;
-    }
-
     const bcpass = bcrypt.hashSync(password, 10);
-
     try {
         await usersCollection.insertOne({ name, password: bcpass, email });
         res.status(201).send('Usuario cadastrado com sucesso')
--- src/middleware/signUpValidate.js
+++ src/middleware/signUpValidate.js
@@ -1,3 +1,20 @@
-export default function signUpValidate() {
-    
+import { registerSchema } from "../tools/JoiSchema.js";
+import { usersCollection } from "../app.js";
+
+export default async function signUpValidate(req, res, next) {
+    const { name, email, password } = req.body;
+
+    const { error } = registerSchema.validate({ name, email, password });
+    if (error) {
+        res.status(422).send(error.details.map(error => error.message));
+        return;
+    }
+
+    const registerExists = await usersCollection.findOne({ email });
+    if (registerExists) {
+        res.status(422).send('Este email já está cadastrado');
+        return;
+    }
+
+    next();
 }
